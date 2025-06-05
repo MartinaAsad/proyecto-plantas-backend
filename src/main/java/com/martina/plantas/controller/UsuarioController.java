0233aacfad8e273efa45a4fadb2fad83525409dd
Martina Asad <lasupermartu15@gmail.com>
2025-06-05 20:36:01 -0300

    creacion del controlador para Usuario

---
+++ src/main/java/com/martina/plantas/controller/UsuarioController.java
@@ -0,0 +1,26 @@
+
+package com.martina.plantas.controller;
+
+import com.martina.plantas.dto.request.UsuarioDTO;
+import com.martina.plantas.service.UsuarioServiceImpl;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Martina
+ */
+@RestController
+public class UsuarioController {
+    
+    @Autowired
+    private UsuarioServiceImpl usuarioServiceImpl;
+    
+    @PostMapping("/users")
+    void crearUsuario(@Valid @RequestBody UsuarioDTO u){
+        usuarioServiceImpl.crearUsuario(u);
+    }
+}
