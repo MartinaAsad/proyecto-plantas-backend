f1e7ca858953e8d5090440f87668b6347b4eb9df
Martina Asad <lasupermartu15@gmail.com>
2025-06-09 14:48:34 -0300

    FEAT (PlantaServiceImpl): Funciona altaPlanta

--- src/main/java/com/martina/plantas/service/PlantaServiceImpl.java
+++ src/main/java/com/martina/plantas/service/PlantaServiceImpl.java
@@ -3,8 +3,12 @@ package com.martina.plantas.service;
 
 import com.martina.plantas.dto.request.PlantaDTO;
 import com.martina.plantas.entities.Planta;
+import com.martina.plantas.entities.Usuario;
+import com.martina.plantas.exception.PlantaExistingException;
+import com.martina.plantas.exception.UsuarioErrorException;
 import com.martina.plantas.mapper.PlantasMapper;
 import com.martina.plantas.repository.PlantaRepository;
+import com.martina.plantas.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +22,9 @@ public class PlantaServiceImpl implements PlantaService {
     @Autowired
     private PlantaRepository plantaRepository;
     
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    
     private final PlantasMapper plantasMapper;
     
     @Autowired
@@ -29,44 +36,45 @@ public class PlantaServiceImpl implements PlantaService {
     public void crearPlanta(PlantaDTO dto) {
         Planta p=plantasMapper.aEntidad(dto);
         
-//        if(!(verificarNulos(dto) && verificarPlanta(dto))){
-//            plantaRepository.save(p);
-//        }
+       if(!(verificarNulos(dto) && verificarPlanta(dto))){
+            plantaRepository.save(p);
+        }
    
     }
     
-//    public boolean verificarNulos(PlantaDTO dto){
-//        boolean nulo=true;
-//        
-//        if(dto.getNombrePlanta().isEmpty()){
-//            throw new UsuarioErrorException("Por favor ingrese el nombre de la planta");
-//        }else{
-//            nulo=false;
-//        }
-//        
-//         if(dto.getDueno()==null){
-//            throw new UsuarioErrorException("Por favor asigne un dueno a la planta");
-//        }else{
-//            nulo=false;
-//        }
-//         
-//          if(dto.getPais()==null){
-//            throw new UsuarioErrorException("Por favor asigne un pais a la planta");
-//        }else{
-//            nulo=false;
-//        }
-//        
-//        
-//        return nulo;
-//    }
-//    public boolean verificarPlanta(PlantaDTO dto){
-//     boolean verificar=true;
-//        if(plantaRepository.existsPlantaByNombrePlantaAndUsuarioidUsuario(dto.getNombrePlanta(), dto.getDueno())){
-//            throw new PlantaExistingException("Ya existe la planta "+dto.getNombrePlanta()+" en este usuario.");
-//        }   else{
-//            verificar=false;
-//        }
-//        return verificar;
-//    }
+    public boolean verificarNulos(PlantaDTO dto){
+        boolean nulo=true;
+        
+        if(dto.getNombrePlanta().isEmpty()){
+            throw new UsuarioErrorException("Por favor ingrese el nombre de la planta");
+        }else{
+            nulo=false;
+        }
+        
+         if(dto.getDueno()==null){
+            throw new UsuarioErrorException("Por favor asigne un dueno a la planta");
+        }else{
+            nulo=false;
+        }
+         
+          if(dto.getIdPais()==null){
+            throw new UsuarioErrorException("Por favor asigne un pais a la planta");
+        }else{
+            nulo=false;
+        }
+        
+        
+        return nulo;
+    }
+    public boolean verificarPlanta(PlantaDTO dto){
+     boolean verificar=true;
+     Usuario u=usuarioRepository.findById(dto.getDueno()).orElseThrow(()-> new UsuarioErrorException("No existe el dueno"));
+        if(plantaRepository.existsPlantaByNombrePlantaAndUsuarioidUsuario(dto.getNombrePlanta(), u)){
+            throw new PlantaExistingException("Ya existe la planta "+dto.getNombrePlanta()+" en este usuario.");
+        }   else{
+            verificar=false;
+        }
+        return verificar;
+    }
     
 }
