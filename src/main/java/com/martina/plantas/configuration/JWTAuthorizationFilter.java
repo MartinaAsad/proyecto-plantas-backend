701e21d1ff60c55ba8ee05518f864a5fda2c6af5
Martina Asad <lasupermartu15@gmail.com>
2025-06-19 18:01:04 -0300

    REFACTOR(JWTAuthorizationFilter):Se añadie anotacion para que el codigo compile

--- src/main/java/com/martina/plantas/configuration/JWTAuthorizationFilter.java
+++ src/main/java/com/martina/plantas/configuration/JWTAuthorizationFilter.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 /**
@@ -20,6 +21,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
  * @author Martina
  */
 @Data
+@Component
 public class JWTAuthorizationFilter extends OncePerRequestFilter {
 
      private final UserDetailsService userDetailsService;
